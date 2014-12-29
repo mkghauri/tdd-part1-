ed5cc2ebb177c2f8ba8d019a372cc1cdf81798e3
mkghauri <mskghauri@yahoo.com>
2014-12-29 23:50:29 +0500

    Complete First Chapter

--- src/org/test/currency/Dollar.java
+++ src/org/test/currency/Dollar.java
@@ -5,10 +5,14 @@ public class Dollar {
 	int amount=10;
 
 	public Dollar(int amount){
-		
+		this.amount= amount;
 	}
 	
 	void times(int multiplier) {
+		//amount= amount * 2;
+		//amount = amount * multiplier;
+		//reduce duplication
+		amount *= multiplier;
 	}
 
 }
